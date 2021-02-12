ac43e932ddbcc909934f8ef38df936bdf25d6d95
Larry Schultheis <lshults.21.117@gmail.com>
2021-02-12 14:01:35 -0500

    Added basic home page with link

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Home"
+def index():
+    return render_template("index.html", name="Index")
+
